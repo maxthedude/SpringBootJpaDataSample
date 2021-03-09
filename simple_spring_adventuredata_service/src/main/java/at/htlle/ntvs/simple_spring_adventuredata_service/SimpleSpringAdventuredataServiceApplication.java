d3aaf16c3ea05a26f6682b781561e9ba11409c2c
maxthedude <markus.klausz@gmail.com>
2021-03-09 12:58:06 +0100

    Initial Commit for the starter project The other lectures can be found in the branches

---
+++ simple_spring_adventuredata_service/src/main/java/at/htlle/ntvs/simple_spring_adventuredata_service/SimpleSpringAdventuredataServiceApplication.java
@@ -0,0 +1,13 @@
+package at.htlle.ntvs.simple_spring_adventuredata_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringAdventuredataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringAdventuredataServiceApplication.class, args);
+	}
+
+}
---
+++ simple_spring_adventuredata_service/src/test/java/at/htlle/ntvs/simple_spring_adventuredata_service/SimpleSpringAdventuredataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlle.ntvs.simple_spring_adventuredata_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringAdventuredataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
